869875008f56a8f3625b1e990d0cdaf4e4ee2e93
Sergey Ponomarev <stokito@gmail.com>
2022-01-06 14:54:09 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# go-str2bytes
+# go-strbytes
 Golang Bytes slice to string and string to byte slice conversion
+
+## Install
+
+    go get -u github.com/stokito/go-strbytes
+
+## Usage
+
+```go
+	w.Write(StrToBytes("Hello World!"))
+```
---
+++ strbytes.go
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+package strbytes
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// BytesToStr converts byte slice to a string without memory allocation.
+// See https://groups.google.com/forum/#!msg/Golang-Nuts/ENgbUzYvCuU/90yGx7GUAgAJ .
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func BytesToStr(b []byte) string {
+	/* #nosec G103 */
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// StrToBytes converts string to a byte slice without memory allocation.
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func StrToBytes(s string) (b []byte) {
+	/* #nosec G103 */
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	/* #nosec G103 */
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	return b
+}
